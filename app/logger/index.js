d1dfa4537e017625206420c1d4f94588f800f1f1
jfabarca <josefrancisco.abarca@avianca.com>
2017-11-16 22:47:38 -0600

    Logging unhandled exceptions

--- app/logger/index.js
+++ app/logger/index.js
@@ -1,23 +1,52 @@
 const { createLogger, format, transports } = require('winston');
+const { combine, timestamp, label, printf } = format;
 
-const logger = createLogger({
-  level: 'info',
-  format: format.json(),
-  transports: [
-    // Write to all logs with level 'info' and below to file
-    // Write all logs error (and below) to 'error.log'
-    new transports.File({ filename: 'app_error.log', level: 'error' }),
-    new transports.File({ filename: 'app.log'})
-  ]
+var env = process.env.NODE_ENV || 'dev';
+
+const customFormat = printf(info => {
+  return `${info.timestamp} [${info.label}] ${info.level.toUpperCase()}: ${info.message}`;
 });
 
-// If we are not in production then log to the console and 'debug.log'
-if(process.env.NODE_ENV !== 'production') {
-  logger.configure({
-    level: 'debug',
+let logger;
+const productionLog = 'app.log';
+const developmentLog = 'debug.log';
+
+if(process.env.NODE_ENV === 'production') {
+  logger = createLogger({
+    level: 'info',
+    // format: format.json(),
+    format: combine(
+      label({ label: env }),
+      timestamp(),
+      customFormat
+    ),
     transports: [
-      new transports.File({ filename: 'debug.log'}),
-      new transports.Console({ format: format.simple() })
+      // Write all logs error (and below) to 'error.log'
+      // new transports.File({ filename: 'app_error.log', level: 'error' }),
+      // Write to all logs with level 'info' and below to file
+      new transports.File({ filename: productionLog})
+    ],
+    exceptionHandlers: [
+      new transports.File({ filename: productionLog})
+    ]
+  });
+
+} else {
+  // dev, qa, etc
+  logger = createLogger({
+    level: 'info',
+    // format: format.json(),
+    format: combine(
+      label({ label: env }),
+      timestamp(),
+      customFormat
+    ),
+    transports: [
+      new transports.File({ filename: developmentLog}),
+      new transports.Console({ format: combine( label({ label: 'console' }), timestamp(), customFormat ) })
+    ],
+    exceptionHandlers: [
+      new transports.File({ filename: developmentLog})
     ]
   });
 }
